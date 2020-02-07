a9b23a935c547077f82fe2959a45462ff3ce2f88
robo-monk <filtap007@gmail.com>
2020-02-07 13:58:09 +0100

    added restart button

--- master.rb
+++ master.rb
@@ -13,22 +13,32 @@ end
 
 get '/game' do
   settings.turns += 1
-  c1 = params['c1'].to_i
-  c2 = params['c2'].to_i
-  c3 = params['c3'].to_i
-  c4 = params['c4'].to_i
-  # puts c1,c2,c3,c4
-  
-  if settings.turns < 11 
-    settings.game.edit_board(settings.turns, [c1,c2,c3,c4])
+  c1 = params['c1']
+  c2 = params['c2']
+  c3 = params['c3']
+  c4 = params['c4']
+  if c1.nil? || c2.nil? || c3.nil? || c4.nil?
+    restart
     out = settings.game.board.render
   else
-    out = 'you suckkk :('
+    if settings.turns < 11 
+      settings.game.edit_board(settings.turns, [c1.to_i,c2.to_i,c3.to_i,c4.to_i])
+      out = settings.game.board.render
+    else
+      out = 'you suckkk :('
+    end
+    if settings.game.won?
+      out = "you won!"
+    end
   end
-  if settings.game.won?
-    out = "you won!"
-  end
-  # col1 = 'red'
-  # puts b.render
   erb :game, :locals => {:board => out}
+end
+
+get '/about' do
+  erb :about
+end
+
+def restart
+  settings.turns = -1
+  settings.game = Game.new([rand(8),rand(8),rand(8),rand(8)])
 end
